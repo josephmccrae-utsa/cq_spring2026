4bd7882ad15b486ca202f783eb3126c77592732c
josephmccrae-utsa <joseph.mccrae@my.utsa.edu>
2026-03-28 10:08:51 -0500

    Commit and push project

---
+++ src/main/java/edu/utsa/cs3443/codequantumspring2026/Launcher.java
@@ -0,0 +1,10 @@
+package edu.utsa.cs3443.codequantumspring2026;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Commiting");
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module edu.utsa.cs3443.codequantumspring2026 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens edu.utsa.cs3443.codequantumspring2026 to javafx.fxml;
+    exports edu.utsa.cs3443.codequantumspring2026;
+}
