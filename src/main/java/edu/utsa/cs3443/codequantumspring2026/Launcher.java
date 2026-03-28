c5e0b432565f0d9d9fc3d99fe3b4c30aeb836416
Dominic Unruh <dominic.unruh@my.utsa.edu>
2026-03-28 10:19:31 -0500

    Dominic's edit

--- src/main/java/edu/utsa/cs3443/codequantumspring2026/Launcher.java
+++ src/main/java/edu/utsa/cs3443/codequantumspring2026/Launcher.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 public class Launcher {
     public static void main(String[] args) {
         System.out.println("Commiting change from Dominic in the branch");
+        System.out.println(System.getProperty("user.dir"));
         Application.launch(HelloApplication.class, args);
     }
 }
