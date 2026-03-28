9f7a6622990a6d443685af18be05ed552cd5d452
josephmccrae-utsa <joseph.mccrae@my.utsa.edu>
2026-03-28 10:36:43 -0500

    Commit and push project

--- src/main/java/edu/utsa/cs3443/codequantumspring2026/Launcher.java
+++ src/main/java/edu/utsa/cs3443/codequantumspring2026/Launcher.java
@@ -6,6 +6,8 @@ public class Launcher {
     public static void main(String[] args) {
         System.out.println("Commiting change from Dominic in the branch");
         //System.out.println(System.getProperty("user.dir"));
+
+        System.out.println("Belinda was here");
         Application.launch(HelloApplication.class, args);
     }
 }
